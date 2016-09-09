c5622af000c74e9ef9c0bd83f2939d413a9c0c88
Klayton Junior <klaytonjunior@hotmail.com>
2016-09-09 09:30:40 -0300

    Exercicio 6 finalizado

--- src/Lista1/Exerc6.java
+++ src/Lista1/Exerc6.java
@@ -10,54 +10,55 @@ public class Exerc6{
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("Digite o N-esimo termo da serie 1, 4, 9, 16...");
-		int a = input.nextInt();
+		int nEsimo = input.nextInt();
 		
-		int c=0;
-		int soma=0,mult=0;
-		
-		while(c<=a-1){
-			imprimiSerie(serie(a,c), a);
-			soma = somaPares(serie(a,c),a);
-			mult = multImpares(serie(a,c),a);
-			c++;
-		}
-		System.out.println("A soma dos pares eh: "+soma);
-		System.out.println("A multiplicaçao dos impares eh: "+mult);
+		int serie[] = serie(nEsimo);
+		int soma = somaSerPares(serie, nEsimo);
+		int mult = multSerImpares(serie, nEsimo);
+		imprimi(serie, soma, mult, nEsimo);
 	}
-	static int serie(int param, int pos){
-		int[] ser = new int[param];
-		int n=0;
-		while(n<=param-1){
-			if(n==0)
-				ser[0]=1;
+	static int[] serie(int n){
+		int[] serie = new int[n];
+		int a=0;
+		while(a<n){
+			serie[a]=(a+1)*(a+1);
+			a++;
+		}
+		return serie;
+	}
+	static int somaSerPares(int[] serie, int n){
+		int a=0;
+		int soma=0;
+		while(a<n){
+			if(serie[a]%2==0){
+				soma = soma + serie[a];
+			}
+			a++;
+		}
+		return soma;
+	}
+	static int multSerImpares(int[] serie, int n){
+		int a=0;
+		int mult=1;
+		while(a<n){
+			if(!(serie[a]%2==0)){
+				mult = mult * serie[a];
+			}
+			a++;
+		}
+		return mult;
+	}
+	static void imprimi(int[] serie, int soma, int mult, int n){
+		int a=0;
+		System.out.print("Esta eh a serie ");
+		while(a<n){
+			if(serie[a]==serie[n-1])
+				System.out.print(serie[a]);
 			else
-				ser[n] = (n+1)*(n+1);
-			n++;
+				System.out.print(serie[a]+", ");
+			a++;
 		}
-		return ser[pos];
-	}
-	static int somaPares(int serie, int param){
-		int seriePares=0;
-		if(serie%2==0){
-			seriePares+=seriePares;
-			seriePares = serie;
-		}
-		return seriePares;
-	}
-	static int multImpares(int serie, int param){
-		int serieImpares=0;
-		if(!(serie%2==0)){
-			serieImpares*=serieImpares;
-			serieImpares = serie;
-		}
-		return serieImpares;
-	}
-	static void imprimiSerie(int serie, int param){
-		int b=0;
-		if(b==param-1)
-			System.out.print(serie+".\n");
-			
-		System.out.print(serie+",");
-		b++;
+		System.out.println("\nA soma dos pares eh: "+soma);
+		System.out.println("A multiplicaçao dos impares eh: "+mult);
 	}
 }
