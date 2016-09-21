d5f05ba2f3f4947379d012f785d18ac528b6798f
Klayton Junior <klaytonjunior@hotmail.com>
2016-09-21 18:55:46 -0300

    Terminar em casa

--- src/Lista4/Circulo.java
+++ src/Lista4/Circulo.java
@@ -1,5 +1,19 @@
 package Lista4;
 
-public class Circulo {
+public class Circulo extends FormaGeometrica{
+	
+	public Circulo(double raio) {
+		super(raio);
+	}
+
+	@Override
+	public double calArea() {
+		return 0;
+	}
+
+	@Override
+	public double calPerim() {// msm coisa q a circunferencia
+		return 2*super.getPi()*super.getLado();
+	}
 
 }
